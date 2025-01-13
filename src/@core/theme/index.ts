af059df47d5a3777c2b8ea79db3cce3b6246e86c
janeza2 <lovekangsom.7@gmail.com>
2025-01-13 20:21:13 +0700

    Replace Public Sans font with Mitr font in theme configuration

--- src/@core/theme/index.ts
+++ src/@core/theme/index.ts
@@ -1,5 +1,5 @@
 // Next Imports
-import { Public_Sans } from 'next/font/google'
+import { Mitr } from 'next/font/google'
 
 // MUI Imports
 import type { Theme } from '@mui/material/styles'
@@ -16,7 +16,10 @@ import shadows from './shadows'
 import customShadows from './customShadows'
 import typography from './typography'
 
-const public_sans = Public_Sans({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700', '800', '900'] })
+const mitr = Mitr({
+  subsets: ['latin'],
+  weight: ['200', '300', '400', '500', '600', '700']
+})
 
 const theme = (settings: Settings, mode: SystemMode, direction: Theme['direction']): Theme => {
   return {
@@ -35,7 +38,7 @@ const theme = (settings: Settings, mode: SystemMode, direction: Theme['direction
       }
     },
     shadows: shadows(mode),
-    typography: typography(public_sans.style.fontFamily),
+    typography: typography(mitr.style.fontFamily),
     customShadows: customShadows(mode),
     mainColorChannels: {
       light: '47 43 61',
